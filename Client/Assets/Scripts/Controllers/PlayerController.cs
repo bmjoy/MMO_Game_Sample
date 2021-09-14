265ded9e4e81e13f066c5687388c9ed73f9598ec
Joseph-Cha <75019048+Joseph-Cha@users.noreply.github.com>
2021-09-14 22:54:53 +0900

    Player 이동 수강 완료

---
+++ Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class PlayerController : MonoBehaviour
+{
+    public Grid _grid;
+    public float _speed = 5.0f;
+    Vector3Int _cellPos = Vector3Int.zero;
+    MoveDir _dir = MoveDir.None;
+    bool _isMoving = false;
+    void Start()
+    {
+        Vector3 pos = _grid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetDirInput();
+        UpdatePosition();
+        UpdateIsMoving();
+    }
+
+    // 스르르 이동
+    private void UpdatePosition()
+    {
+        if (_isMoving == false)
+            return;
+
+        Vector3 desPos = _grid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);
+        Vector3 moveDir = desPos - transform.position;
+
+        // 도착 여부 체크
+        float dist = moveDir.magnitude;
+        // 도착 완료
+        if (dist < _speed * Time.deltaTime)
+        {
+            transform.position = desPos;
+            _isMoving = false;
+        }
+        // 도착 전
+        else
+        {
+            transform.position += moveDir.normalized * _speed * Time.deltaTime;
+            _isMoving = true;
+        }
+    }
+
+    // 이동 가능한 상태일 때 실제 좌표 이동
+    private void UpdateIsMoving()
+    {
+        if (_isMoving == false)
+        {
+            switch (_dir)
+            {
+                case MoveDir.Up:
+                    _cellPos += Vector3Int.up;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Down:
+                    _cellPos += Vector3Int.down;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Right:
+                    _cellPos += Vector3Int.right;
+                    _isMoving = true;
+                    break;
+                case MoveDir.Left:
+                    _cellPos += Vector3Int.left;
+                    _isMoving = true;
+                    break;
+            }
+        }    
+    }
+
+    // 키보드 입력 후 반향
+    void GetDirInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _dir = MoveDir.Up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _dir = MoveDir.Down;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _dir = MoveDir.Left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _dir = MoveDir.Right;
+        }
+        else
+        {
+            _dir = MoveDir.None;
+        }
+    }
+}
