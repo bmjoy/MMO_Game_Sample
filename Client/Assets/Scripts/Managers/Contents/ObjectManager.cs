2aeb92e79cc8813b0edc7f98a806990db05fdeac
Joseph-Cha <75019048+Joseph-Cha@users.noreply.github.com>
2021-09-20 13:18:35 +0900

    오브젝트 매니저 수강완료

---
+++ Client/Assets/Scripts/Managers/Contents/ObjectManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager 
+{
+    // Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject>();
+    List<GameObject> _object = new List<GameObject>();
+
+    public void Add(GameObject go)
+    {
+        _object.Add(go);
+    }
+    public void Remove(GameObject go)
+    {
+        _object.Remove(go);
+    }
+
+    public GameObject Find(Vector3Int cellPos)
+    {
+        foreach (GameObject obj in _object)
+        {
+            CreatureController cc = obj.GetComponent<CreatureController>();
+            if (cc == null)
+                continue;
+            if (cc.CellPos == cellPos)
+                return obj;
+        }
+        return null;
+    }
+    
+    public void Clear()
+    {
+        _object.Clear();
+    }
+}
