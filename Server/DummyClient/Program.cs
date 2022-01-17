15175c6a6c06017a32d5bb9a98e498bc05208783
Joseph.c <joseph.c@kakao.com>
2022-01-17 14:48:27 +0900

    더미 클라이언트 작업 #1

---
+++ Server/DummyClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using DummyClient.Session;
+using ServerCore;
+
+namespace DummyClient
+{
+    class Program
+    {
+        static int DummyClientCount { get; } = 500;
+
+        static void Main(string[] args)
+		{
+            Thread.Sleep(3000);
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[1];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Connector connector = new Connector();
+
+            connector.Connect(endPoint,
+                () => SessionManager.Instance.Generate(),
+                DummyClientCount);
+            
+            while (true)
+            {
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
---
+++ Server/DummyClient/Session/SessionManager.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace DummyClient.Session
+{
+    // 여러 더미 클라이언트들이 각자 하나의 서버와 연결이 되어 있는 상태를 관리
+    public class SessionManager
+    {
+        public static SessionManager Instance { get; } = new SessionManager();
+
+        HashSet<ServerSession> _sessions = new HashSet<ServerSession>();
+        object _lock = new Object();
+        int _dummyId = 1;
+
+        public ServerSession Generate()
+        {
+            lock (_lock)
+            {
+                ServerSession session = new ServerSession();
+                session.DummyId = _dummyId;
+                _dummyId++;
+
+                _sessions.Add(session);
+                Console.WriteLine($"Connected ({_sessions.Count}) Players");
+                return session;
+            }
+        }
+
+        public void Remove(ServerSession session)
+        {
+            lock (_lock)
+            {
+                _sessions.Remove(session);
+                Console.WriteLine($"Disconnected ({_sessions.Count}) Players");
+            }
+        }
+    }
+}
